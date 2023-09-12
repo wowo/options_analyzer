000441a06d08be0a132ae04e6b361cf842ad738a
Wojciech Sznapka <wojciech@sznapka.pl>
2023-09-12 23:25:16 +0200

    Fetching top 200 symbols from Seeking Alpha for analysis

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-
-import pytz
 from pandas import DataFrame
 from supabase import create_client, Client
 import inflection
+import json
 import os
+import pytz
+import requests
 import yfinance as yf
 
 STOCK_COLUMNS = ['symbol', 'industry', 'industryDisp', 'longName', 'recommendationKey', 'sector', 'sectorDisp',
@@ -28,23 +29,46 @@ STOCK_COLUMNS = ['symbol', 'industry', 'industryDisp', 'longName', 'recommendati
                  'trailingAnnualDividendYield', 'trailingEps', 'trailingPE', 'trailingPegRatio', 'twoHundredDayAverage',
                  'volume']
 
+
+def fetch_top_quant_from_seeking_alpha():
+    url = 'https://seekingalpha.com/api/v3/screener_results'
+    payload = json.dumps({
+        'per_page': 200,
+        'sort': '-quant_rating',
+        'type': 'stock'
+    })
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0',
+        'Content-Type': 'application/json',
+    }
+    response = requests.request('POST', url, headers=headers, data=payload)
+    return [x['attributes']['name'] for x in response.json()['data']]
+
+
+def get_tickers_for_analysis():
+    tickers = ['DUOL', 'ASC', 'PERI', 'ADYEY', 'JNPR', 'LUV', 'CNHI', 'AGCO', 'NU', 'CRWD', 'PYPL', 'TM',
+               'SONY', 'EA', 'TTWO', 'MA', 'INTU', 'V', 'TEAM', 'HUBS', 'ADBE', 'AMD', 'GMVHF', 'BBAI', 'IMMR',
+               'SMCI', 'SPLK', 'UBER', 'SPOT', 'RIVN', 'EWBC', 'PSNY', 'META', 'RBLX', 'SMWB', 'MED', 'NVDA',
+               'INTC', 'FLIC', 'MMM', 'MO', 'ELBM', 'PLTR', 'CIOXY', 'BIDU', 'TCEHY', 'BABA', 'ASML', 'MNDY',
+               'DOCN', 'GDDY', 'SHOP', 'MDB', 'OKTA', 'AKAM', 'FSLY', 'AFRM', 'AAPL', 'MPWR', 'SGHC',
+               'NET', 'SNOW', 'DDOG', 'OTGLF', 'ABNB', 'DIS', 'NFLX', 'MSFT', 'CRM', 'AMZN', 'GOOG', 'RYAAY']
+    tickers = tickers + fetch_top_quant_from_seeking_alpha()
+
+    return set(tickers)
+
+
 if __name__ == '__main__':
     supabase: Client = create_client(
         os.environ.get("SUPABASE_URL"),
         os.environ.get("SUPABASE_KEY")
     )
 
-    ticker_names = ['DUOL', 'ASC', 'PERI', 'ADYEY', 'JNPR', 'LUV', 'CNHI', 'AGCO', 'NU', 'CRWD', 'PYPL', 'TM',
-                    'SONY', 'EA', 'TTWO', 'MA', 'INTU', 'V', 'TEAM', 'HUBS', 'ADBE', 'AMD', 'GMVHF', 'BBAI', 'IMMR',
-                    'SMCI', 'SPLK', 'UBER', 'SPOT', 'RIVN', 'EWBC', 'PSNY', 'META', 'RBLX', 'SMWB', 'MED', 'NVDA',
-                    'INTC', 'FLIC', 'MMM', 'MO', 'ELBM', 'PLTR', 'CIOXY', 'BIDU', 'TCEHY', 'BABA', 'ASML', 'MNDY',
-                    'DOCN', 'GDDY', 'SHOP', 'MDB', 'OKTA', 'AKAM', 'FSLY', 'AFRM', 'AAPL', 'MPWR', 'SGHC',
-                    'NET', 'SNOW', 'DDOG', 'OTGLF', 'ABNB', 'DIS', 'NFLX', 'MSFT', 'CRM', 'AMZN', 'GOOG']
+    symbols = get_tickers_for_analysis()
 
-    for ticker_name in ticker_names:
+    for symbol in symbols:
         try:
-            print(f'Fetching ticker {ticker_name}')
-            ticker = yf.Ticker(ticker_name)
+            print(f'Fetching symbol {symbol}')
+            ticker = yf.Ticker(symbol)
             info = ticker.info
             stock_data = {inflection.underscore(k): info[k] for k in STOCK_COLUMNS if k in info}
             stock_data['updated_at'] = datetime.now().isoformat()
@@ -52,7 +76,7 @@ if __name__ == '__main__':
 
             expirations = ticker.options
             for expiration in expirations[:3]:
-                print(f'Fetching options chain for ticker {ticker_name} expiration {expiration}')
+                print(f'Fetching options chain for symbol {symbol} expiration {expiration}')
                 puts: DataFrame = ticker.option_chain(expiration).puts
 
                 underscore_cols = {}
@@ -65,7 +89,7 @@ if __name__ == '__main__':
                 for index, row in puts.iterrows():
                     row_data = row.to_dict()
                     row_data['last_trade_date'] = row_data['last_trade_date'].isoformat()
-                    row_data['symbol'] = ticker_name
+                    row_data['symbol'] = symbol
                     row_data['expiration'] = expiration
                     row_data['updated_at'] = datetime.now().astimezone(pytz.timezone('Poland')).isoformat()
 
