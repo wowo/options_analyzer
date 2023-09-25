7797e4ba72972dae948f894c3ecb1049ae054029
Wojciech Sznapka <wojciech@sznapka.pl>
2023-09-25 21:55:09 +0200

    Cloud function http, added logging

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-import os
-from google.cloud import pubsub_v1
 from flask import Request
-from utils import get_symbols_from_database
+from google.cloud import pubsub_v1
 from supabase import create_client
+from utils import get_symbols_from_database
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO)
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(
+    os.environ.get('GCP_PROJECT_ID'),
+    os.environ.get('GCP_TOPIC_ID'),
+)
+
+supabase = create_client(
+    os.environ.get('SUPABASE_URL'),
+    os.environ.get('SUPABASE_KEY')
+)
 
 
 def publish_symbols_to_analyze(request: Request):
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(
-        os.environ.get('GCP_PROJECT_ID'),
-        os.environ.get('GCP_TOPIC_ID'),
-    )
-
-    supabase = create_client(
-        os.environ.get('SUPABASE_URL'),
-        os.environ.get('SUPABASE_KEY')
-    )
-
     try:
         symbol = request.args.get('symbol')
         symbols = get_symbols_from_database(supabase)
         if symbol:
             symbols.append(symbol)
+        logging.info(f'Starting to publishing, all: {len(symbols)} new symbol: {symbol}')
         for symbol in symbols:
             message_future = publisher.publish(topic_path, symbol.encode('utf-8'))
             message_future.result()
+        logging.info(f'Published')
 
         return f"Published {len(symbols)} symbols.", 200
     except Exception as e:
