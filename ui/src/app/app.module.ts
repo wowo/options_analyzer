d8efbaa5894798e676deae6fcc97e2ff85fa44e3
Wojciech Sznapka <wojciech@sznapka.pl>
2023-11-01 09:12:42 +0100

    feat(ui): list view WIP

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
